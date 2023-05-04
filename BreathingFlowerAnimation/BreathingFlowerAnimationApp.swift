2552feb0dfc27bb0f7069078006b7fd8448794fc
Asadbek <asadbekn443@gmail.com>
2023-05-04 07:41:40 -0400

    Initial Commit

---
+++ BreathingFlowerAnimation/BreathingFlowerAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathingFlowerAnimationApp.swift
+//  BreathingFlowerAnimation
+//
+//  Created by Asadbek Nematov on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BreathingFlowerAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
